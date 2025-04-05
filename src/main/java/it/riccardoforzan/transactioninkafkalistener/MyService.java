d18c7e8220995084345359e64be57944bf2ab3cc
riccardoforzan <37451892+riccardoforzan@users.noreply.github.com>
2025-04-05 11:38:00 +0200

    Initial commit

---
+++ src/main/java/it/riccardoforzan/transactioninkafkalistener/MyService.java
@@ -0,0 +1,4 @@
+package it.riccardoforzan.transactioninkafkalistener;
+
+public class MyService {
+}
---
+++ src/main/java/it/riccardoforzan/transactioninkafkalistener/TransactionInKafkaListenerApplication.java
@@ -0,0 +1,13 @@
+package it.riccardoforzan.transactioninkafkalistener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionInKafkaListenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionInKafkaListenerApplication.class, args);
+	}
+
+}
